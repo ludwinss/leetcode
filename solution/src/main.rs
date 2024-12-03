8b092d112b3b6480258c5a0c47dc48ce2a2991e8
Ludwin FM <ludwinss@gmail.com>
2024-12-03 01:22:11 -0400

    989.Add_To_Array_Form_Of_Integer

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,20 +1,27 @@
 fn main() {
-    let height: Vec<i32> = vec![1, 2, 3, 4, 5];
-    let treshold = 2;
+    let num = vec![2, 1, 5];
+    let k = 806;
 
-    let solutions = stable_mountains(height, treshold);
+    let solutions = add_to_array_form(num, k);
     println!("{:?}", solutions);
 }
 
-pub fn stable_mountains(height: Vec<i32>, threshold: i32) -> Vec<i32> {
-    let mut mountains = Vec::new();
-    if height.len() <= 1 {
-        return mountains;
+pub fn add_to_array_form(num: Vec<i32>, k: i32) -> Vec<i32> {
+    let mut carry = k;
+    let mut result = Vec::new();
+
+    for digit in num.iter().rev() {
+        carry += *digit;
+        result.push(carry % 10);
+        carry /= 10;
     }
-    for idx_mountain in 1..=height.len() - 1 {
-        if height[idx_mountain - 1] > threshold {
-            mountains.push(idx_mountain as i32);
-        }
+
+    while carry > 0 {
+        result.push(carry % 10);
+        carry /= 10;
     }
-    mountains
+
+    result.reverse();
+
+    result
 }
