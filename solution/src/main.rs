bc73ca08b2783f56827f51df8e785ac3e6b800f9
Ludwin FM <ludwinss@gmail.com>
2024-12-07 11:17:30 -0400

    wtf with this problem

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,56 +1,30 @@
-fn main() {
-    let input = vec![
-        "flower".to_string(),
-        "flow".to_string(),
-        "flight".to_string(),
-    ];
+use std::collections::HashSet;
 
-    let solutions = longest_common_prefix(input);
-    println!("{:?}", solutions);
+fn main() {
+    let ex1 = vec![46, 8, 2, 4, 1, 4, 10, 2, 4, 10, 2, 5, 7, 3, 1];
+    let ex2 = vec![1, 2, 3, 2, 5];
+    let ex3 = vec![3, 4, 5, 1, 12, 14, 13];
+    let ex4 = vec![38];
+    let ex5 = vec![1, 2, 3, 9, 2, 10, 8, 3, 10, 2];
+
+    assert_eq!(missing_integer(ex1), 47);
+    assert_eq!(missing_integer(ex2), 6);
+    assert_eq!(missing_integer(ex3), 15);
+    assert_eq!(missing_integer(ex4), 39);
+    assert_eq!(missing_integer(ex5), 6);
 }
 
-pub fn longest_common_prefix(strs: Vec<String>) -> String {
-    if strs.is_empty() {
-        return "".to_string();
+pub fn missing_integer(x: Vec<i32>) -> i32 {
+    // Honestly the problem sucks
+    let a = x.iter().cloned().collect::<HashSet<_>>();
+    let mut i = 1;
+    let mut total = x[0];
+    while i < x.len() && x[i] == x[i - 1] + 1 {
+        total += x[i];
+        i += 1;
     }
-
-    // avoid to modify the original vector
-    //let mut order_vect_prefix: Vec<String> = strs.clone();
-
-    // not sort just to get the shortest string
-    //order_vect_prefix.sort_by_key(|chars| chars.len());
-    let mut posibily_perfix = strs
-        .iter()
-        .min_by_key(|posible_short_string| posible_short_string.len())
-        .unwrap()
-        .as_str();
-
-    // avoid clon vector
-    //let word = String::from(order_vect_prefix[0].clone());
-
-    // modified loop and improve conditional to break
-    // for idx_next_char in (1..=order_vect_prefix.len() + 1).rev() {
-    //     order_vect_prefix.iter().for_each(|string_find| {
-    //         let word_slice: String = string_find.chars().take(idx_next_char).collect();
-    //         if word_slice == word.chars().take(idx_next_char).collect::<String>() {
-    //             idx_common = idx_next_char;
-    //         } else {
-    //             idx_common = 0;
-    //         }
-    //     });
-    //     if idx_common != 0 {
-    //         break;
-    //     }
-    // }
-
-    for next_perfix in strs.iter() {
-        while !next_perfix.starts_with(&posibily_perfix) {
-            posibily_perfix = &posibily_perfix[0..posibily_perfix.len() - 1];
-            if posibily_perfix.is_empty() {
-                return String::from("");
-            }
-        }
+    while a.contains(&total) {
+        total += 1
     }
-
-    posibily_perfix.to_string()
+    total
 }
