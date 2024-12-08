8ef87499f93276e0ab7c6226ee45d807b878bc10
Ludwin FM <ludwinss@gmail.com>
2024-12-08 10:38:20 -0400

    my best solution until now

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -6,14 +6,19 @@ fn main() {
     let input = vec![1, 2, 3, 4, 3];
 
     assert_eq!(next_greater_element(input), vec![2, 3, 4, -1, 4]);
+    println!("All test passed!");
 }
 
 pub fn next_greater_element(nums1: Vec<i32>) -> Vec<i32> {
-    let circle_elements = Vec::from([nums1.clone(), nums1.clone()].concat());
-    let mut stack: Vec<i32> = vec![];
-    let mut result: Vec<i32> = vec![-1; circle_elements.len()];
+    // avoid use clone an duplicate the memory
+    //let circle_elements = Vec::from([nums1.clone(), nums1.clone()].concat());
+    let total_len = nums1.len();
+    let mut stack: Vec<i32> = Vec::with_capacity(total_len);
+    let mut result: Vec<i32> = vec![-1; total_len];
 
-    for (idx, &num) in circle_elements.iter().enumerate().rev() {
+    for idx in (0..total_len * 2).rev() {
+        // use properties o base number to extract the number in nums1
+        let num = nums1[idx % total_len];
         while let Some(&header_value) = stack.last() {
             if header_value <= num {
                 stack.pop();
@@ -22,13 +27,15 @@ pub fn next_greater_element(nums1: Vec<i32>) -> Vec<i32> {
             }
         }
 
-        if let Some(&top) = stack.last() {
-            result[idx] = top
+        // if idx is less than total_len, then we can store the result
+        if idx < total_len {
+            if let Some(&total) = stack.last() {
+                result[idx] = total;
+            }
         }
 
         stack.push(num)
     }
 
-    result.truncate(nums1.len());
     result
 }
