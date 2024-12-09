85229f6b5ca45c82c4f82fce030270d24786a81b
Ludwin FM <ludwinss@gmail.com>
2024-12-09 00:27:45 -0400

    my first solution

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,41 +1,29 @@
 fn main() {
-    let input = vec![1, 2, 1];
+    let haystack = "sadbutsad".to_string();
+    let needle = "sad".to_string();
 
-    assert_eq!(next_greater_element(input), vec![2, -1, 2]);
+    assert_eq!(str_str(haystack, needle), 0);
 
-    let input = vec![1, 2, 3, 4, 3];
+    let haystack = "leetcode".to_string();
+    let needle = "leeto".to_string();
 
-    assert_eq!(next_greater_element(input), vec![2, 3, 4, -1, 4]);
+    assert_eq!(str_str(haystack, needle), -1);
+
     println!("All test passed!");
 }
 
-pub fn next_greater_element(nums1: Vec<i32>) -> Vec<i32> {
-    // avoid use clone an duplicate the memory
-    //let circle_elements = Vec::from([nums1.clone(), nums1.clone()].concat());
-    let total_len = nums1.len();
-    let mut stack: Vec<i32> = Vec::with_capacity(total_len);
-    let mut result: Vec<i32> = vec![-1; total_len];
+pub fn str_str(haystack: String, needle: String) -> i32 {
+    let max_len_haystack: usize = haystack.chars().map(|cha| cha.len_utf8()).sum();
+    let max_len_needle: usize = needle.chars().map(|cha| cha.len_utf8()).sum();
 
-    for idx in (0..total_len * 2).rev() {
-        // use properties o base number to extract the number in nums1
-        let num = nums1[idx % total_len];
-        while let Some(&header_value) = stack.last() {
-            if header_value <= num {
-                stack.pop();
-            } else {
-                break;
-            }
+    for idx in 0..max_len_haystack {
+        if idx + max_len_needle > max_len_haystack {
+            break;
         }
 
-        // if idx is less than total_len, then we can store the result
-        if idx < total_len {
-            if let Some(&total) = stack.last() {
-                result[idx] = total;
-            }
+        if &haystack[idx..max_len_needle + idx] == needle {
+            return idx as i32;
         }
-
-        stack.push(num)
     }
-
-    result
+    -1
 }
