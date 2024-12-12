22134a69955a447c71fbf218c295e40f33289247
Ludwin FM <ludwinss@gmail.com>
2024-12-11 21:15:58 -0400

    my first solution

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,70 +1,33 @@
-use std::u8;
-
 fn main() {
-    let haystack = "sadbutsad".to_string();
-    let needle = "sad".to_string();
+    let input = 123;
+    assert_eq!(reverse(input), 321);
 
-    assert_eq!(str_str(haystack, needle), 0);
+    let input = -123;
 
-    let haystack = "leetcode".to_string();
-    let needle = "leeto".to_string();
-
-    assert_eq!(str_str(haystack, needle), -1);
-
-    let haystack = "aabaaabaaac".to_string();
-    let needle = "aabaaac".to_string();
-
-    assert_eq!(str_str(haystack, needle), 4);
+    assert_eq!(reverse(input), -321);
 
     println!("All test passed!");
 }
 
-pub fn str_str(haystack: String, needle: String) -> i32 {
-    let haystack_bytes = haystack.as_bytes();
-    let needle_bytes = needle.as_bytes();
+pub fn reverse(x: i32) -> i32 {
+    let is_negative = x < 0;
 
-    let lsp_vector = create_lsp(needle_bytes);
+    let x = x.abs();
 
-    println!("{:?}",lsp_vector);
+    let x_invert: String = x
+        .to_string()
+        .chars()
+        .rev()
+        .fold(String::new(), |mut acc, char| {
+            acc.push(char);
+            acc
+        });
 
-    let mut idx_needle = 0;
-    let mut idx_haystack = 0;
+    let x_invert = x_invert.parse::<i32>().unwrap_or(0);
 
-    while idx_haystack < haystack_bytes.len() {
-        if haystack_bytes[idx_haystack] == needle_bytes[idx_needle] {
-            idx_needle += 1;
-            idx_haystack += 1;
-            if idx_needle == needle_bytes.len() {
-                return (idx_haystack - idx_needle) as i32;
-            }
-        } else {
-            if idx_needle > 0 {
-                idx_needle = lsp_vector[idx_needle - 1];
-            } else {
-                idx_haystack += 1;
-            }
-        }
-    }
-    -1
-}
-
-pub fn create_lsp(pattern: &[u8]) -> Vec<usize> {
-    let mut lsp_vector: Vec<usize> = vec![0; pattern.len()];
-    let mut length = 0;
-    let mut idx = 1;
-
-    while idx < pattern.len() {
-        if pattern[idx] == pattern[length] {
-            length += 1;
-            lsp_vector[idx] = length;
-            idx += 1;
-        } else if length > 0 {
-            length = lsp_vector[length - 1];
-        } else {
-            lsp_vector[idx] = 0;
-            idx += 1;
-        }
+    if is_negative {
+        return x_invert * -1;
     }
 
-    lsp_vector
+    x_invert
 }
