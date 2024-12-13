7e170666b46b4d2c6600e83a1c91ca9b4dbd11da
Ludwin FM <ludwinss@gmail.com>
2024-12-13 01:44:16 -0400

    my best solution until now

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -8,30 +8,30 @@ fn main() {
     let input = vec![30, 60, 90];
     assert_eq!(daily_temperatures(input), vec![1, 1, 0]);
 
+    let input = vec![89, 62, 70, 58, 47, 47, 46, 76, 100, 70];
+    assert_eq!(
+        daily_temperatures(input),
+        vec![8, 1, 5, 4, 3, 2, 1, 1, 0, 0]
+    );
+
     println!("All test passed!");
 }
 pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
     let mut result = vec![0; temperatures.len()];
-    let mut stack: Vec<(i32, i32)> = Vec::with_capacity(temperatures.len());
+    let mut stack: Vec<usize> = Vec::with_capacity(temperatures.len());
 
-    for (idx, &temperature) in temperatures.iter().rev().enumerate() {
-        while let Some(&greatest_temperature) = stack.last() {
-            if greatest_temperature.1 <= temperature {
+    for (idx, &temperature) in temperatures.iter().enumerate() {
+        while let Some(&greatest_idx) = stack.last() {
+            if temperatures[greatest_idx] < temperature {
+                result[greatest_idx] = (idx - greatest_idx) as i32;
                 stack.pop();
             } else {
                 break;
             }
         }
 
-        if let Some(&greatest_temperature) = stack.last() {
-            result[idx] = idx as i32 - greatest_temperature.0;
-        }
-
-        stack.push((idx as i32, temperature));
+        stack.push(idx);
     }
-    result.reverse();
-
-    println!("{:?}", result);
 
     result
 }
