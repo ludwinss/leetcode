629ef9dd9d49060312623bd4e555964ba46fcf37
Ludwin FM <ludwinss@gmail.com>
2025-01-15 10:53:02 -0400

    best using XOR

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 fn main() {
     assert_eq!(single_number(vec![1, 2, 3, 1, 3]), 2);
     assert_eq!(single_number(vec![1, 1, 5, 3, 3, 5, 4]), 4);
@@ -7,17 +5,5 @@ fn main() {
 }
 
 pub fn single_number(nums: Vec<i32>) -> i32 {
-    let mut memoization: HashMap<i32, i32> = HashMap::new();
-
-    for digit in &nums {
-        *memoization.entry(*digit).or_insert(0) += 1;
-    }
-
-    for (digit, value) in &memoization {
-        if *value == 1 {
-            return *digit;
-        }
-    }
-
-    0
+    nums.iter().fold(0, |acc, &next| acc ^ next)
 }
