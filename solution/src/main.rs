83654bb8ea6c288a0668e24512e63942953a3de0
Ludwin FM <ludwinss@gmail.com>
2025-01-14 21:59:42 -0400

    worst solution using bit manipulation

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -2,38 +2,29 @@ fn main() {
     assert_eq!(is_power_of_two(1), true);
     assert_eq!(is_power_of_two(16), true);
     assert_eq!(is_power_of_two(3), false);
+    assert_eq!(is_power_of_two(2147483647), false);
     println!("All test passed!");
 }
 
 pub fn is_power_of_two(n: i32) -> bool {
+    let mut next_pow = 1;
     if n == 1 {
         return true;
     }
 
-    if n % 2 !=0 {
+    if n % 2 != 0 {
         return false;
     }
 
-    let mut left = 0;
-    let mut right = n;
-
-    while left <= right {
-        let mid: i32 = left + (right - left)/2;
-
-        let pow_value = (2 as u32).pow(mid as u32) as i32;
-
-        if n.abs() == pow_value {
+    loop {
+        if n.abs() == next_pow {
             return true;
         }
-
-        if pow_value > n.abs() {
-            right = mid -1;
-        } else {
-            left = mid +1;
+        next_pow <<= 1;
+        if next_pow > n {
+            break;
         }
-
-        println!("{:?}, {:?}", left, right);
     }
-
+    println!("left:{}, right:{}", n << 1, n);
     false
 }
