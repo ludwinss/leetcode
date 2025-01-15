92c683912086c29fae614c6a2c8c04ec6934147e
Ludwin FM <ludwinss@gmail.com>
2025-01-14 22:22:01 -0400

    best solution using bit manipulation and AND comparation

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -3,28 +3,11 @@ fn main() {
     assert_eq!(is_power_of_two(16), true);
     assert_eq!(is_power_of_two(3), false);
     assert_eq!(is_power_of_two(2147483647), false);
+    assert_eq!(is_power_of_two(-2147483647), false);
+    assert_eq!(is_power_of_two(-16), false);
     println!("All test passed!");
 }
 
 pub fn is_power_of_two(n: i32) -> bool {
-    let mut next_pow = 1;
-    if n == 1 {
-        return true;
-    }
-
-    if n % 2 != 0 {
-        return false;
-    }
-
-    loop {
-        if n.abs() == next_pow {
-            return true;
-        }
-        next_pow <<= 1;
-        if next_pow > n {
-            break;
-        }
-    }
-    println!("left:{}, right:{}", n << 1, n);
-    false
+    n > 0 && (n & (n - 1)) == 0
 }
