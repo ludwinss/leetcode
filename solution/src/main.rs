2a86e89ad1e47b5adb91e9dbd0d76bf0d04b62e5
Ludwin FM <ludwinss@gmail.com>
2025-01-17 22:43:59 -0400

    worst solution brute force

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,9 +1,25 @@
 fn main() {
-    assert_eq!(single_number(vec![1, 2, 3, 1, 3]), 2);
-    assert_eq!(single_number(vec![1, 1, 5, 3, 3, 5, 4]), 4);
+    assert_eq!(find_max_consecutive_ones(vec![1, 0, 1, 0, 0, 1, 1, 1]), 3);
+    assert_eq!(find_max_consecutive_ones(vec![1, 1, 1, 0, 0, 1]), 3);
+    assert_eq!(find_max_consecutive_ones(vec![0, 1, 1, 0, 0, 1]), 2);
+    assert_eq!(find_max_consecutive_ones(vec![0, 0, 1, 1, 0, 1]), 2);
     println!("All test passed!");
 }
 
-pub fn single_number(nums: Vec<i32>) -> i32 {
-    nums.iter().fold(0, |acc, &next| acc ^ next)
+pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
+    let mut max = 0;
+    let mut count = 0;
+
+    nums.iter().for_each(|&num| {
+        if num == 1 {
+            count += 1;
+        } else {
+            count = 0;
+        }
+        if count > max {
+            max = count;
+        }
+    });
+
+    max
 }
