da1e52bc65cd21b8a78933145dd2a5553baed6f1
Ludwin FM <ludwinss@gmail.com>
2025-03-19 14:48:17 -0400

    solution using sort and loops into numbers

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,19 +1,26 @@
 fn main() {
-    let input = vec![2, 2, 3, 2];
-    assert_eq!(single_number(input), 3);
+    let input = vec![1, 3, 2, 4, 1];
+    assert_eq!(max_ice_cream(input, 7), 4);
 
-    let input = vec![0, 1, 0, 1, 0, 1, 99];
-    assert_eq!(single_number(input), 99);
+    let input = vec![10, 6, 8, 7, 7, 8];
+    assert_eq!(max_ice_cream(input, 5), 0);
 
     println!("All test passed!");
 }
 
-pub fn single_number(nums: Vec<i32>) -> i32 {
-    let mut input1 = 0;
-    let mut input2 = 0;
-    for num in &nums {
-        input1 = (input1 ^ num) & !input2;
-        input2 = (input2 ^ num) & !input1;
+pub fn max_ice_cream(costs: Vec<i32>, coins: i32) -> i32 {
+    let mut cost_mut = costs.clone();
+    cost_mut.sort();
+
+    let (mut count, mut sum) = (0, 0);
+
+    for &cost in cost_mut.iter() {
+        if sum + cost <= coins {
+            count += 1;
+            sum += cost;
+        } else {
+            break;
+        }
     }
-    input1
+    count
 }
