cc4f5a419c1980b9c43827cb6b8a6f3c9c44dd85
Ludwin FM <ludwinss@gmail.com>
2025-07-24 12:18:32 -0400

    solution using hashmap

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,25 +1,33 @@
-fn main() {
-    let num = String::from("(1)+((2))+(((3)))");
+use std::collections::HashMap;
 
-    let solutions = max_depth(num);
-    println!("{:?}", solutions);
+fn main() {
+    let example = String::from("aaaaabbc");
+    assert_eq!(max_difference(example), 3);
+
+    let example = String::from("abcabcab");
+    assert_eq!(max_difference(example), 1);
+
+    println!("All test passed!");
 }
 
-pub fn max_depth(s: String) -> i32 {
-    let mut max_value: i32 = 0;
-    let mut current_depth = 0;
+pub fn max_difference(s: String) -> i32 {
+    let mut hash_value: HashMap<u8, i32> = HashMap::new();
 
-    for char in s.chars() {
-        match char {
-            '(' => {
-                current_depth += 1;
-                max_value = max_value.max(current_depth);
-            }
-            ')' => {
-                current_depth -= 1;
-            }
-            _ => continue
-        }
+    for &value in s.as_bytes().iter() {
+        hash_value
+            .entry(value)
+            .and_modify(|count| *count += 1)
+            .or_insert(1);
     }
-    max_value
+
+    let (min, max) = hash_value
+        .values()
+        .fold((i32::MAX, i32::MIN), |(min, max), &next| {
+            if next == 1 {
+                (min, max)
+            } else {
+                (min.min(next), max.max(next))
+            }
+        });
+    max - min
 }
