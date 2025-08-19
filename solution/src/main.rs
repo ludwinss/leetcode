e2be5671e7c3726a54e67b1d4fe24934cadf0e31
Ludwin FM <ludwinss@gmail.com>
2025-08-19 10:34:57 -0400

    solution using array

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::cmp::{max, min};
 
 fn main() {
     let example = String::from("aaaaabbc");
@@ -7,27 +7,46 @@ fn main() {
     let example = String::from("abcabcab");
     assert_eq!(max_difference(example), 1);
 
+    let example = String::from("aabbbc");
+    assert_eq!(max_difference(example), 1);
+
+    let example = String::from("aaaab");
+    assert_eq!(max_difference(example), -3);
+
+    let example = String::from("aabbc");
+    assert_eq!(max_difference(example), -1);
+
+    let example = String::from("aaabbc");
+    assert_eq!(max_difference(example), 1);
+
+    let example = String::from("zzzzxxxyyy");
+    assert_eq!(max_difference(example), -1);
+
     println!("All test passed!");
 }
 
 pub fn max_difference(s: String) -> i32 {
-    let mut hash_value: HashMap<u8, i32> = HashMap::new();
+    let mut vector_s: Vec<i32> = vec![0; 26];
+    let a = b'a';
 
-    for &value in s.as_bytes().iter() {
-        hash_value
-            .entry(value)
-            .and_modify(|count| *count += 1)
-            .or_insert(1);
+    s.as_bytes()
+        .iter()
+        .for_each(|&char| vector_s[(char - a) as usize] += 1);
+
+    let mut odd = 0;
+    let mut even = i32::MAX;
+
+    for &value in &vector_s {
+        if value % 2 == 1 {
+            odd = max(odd, value);
+        } else if value != 0 {
+            even = min(even, value);
+        }
     }
 
-    let (min, max) = hash_value
-        .values()
-        .fold((i32::MAX, i32::MIN), |(min, max), &next| {
-            if next == 1 {
-                (min, max)
-            } else {
-                (min.min(next), max.max(next))
-            }
-        });
-    max - min
+    if even == i32::MAX {
+        odd
+    } else {
+        odd - even
+    }
 }
