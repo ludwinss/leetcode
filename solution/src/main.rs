24b1161c541794afb2e6f79d7f7c88e77b9eb2bc
Ludwin FM <ludwinss@gmail.com>
2025-10-17 15:38:15 -0400

    solution using counting sort

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,25 +1,35 @@
 fn main() {
-    let num = String::from("(1)+((2))+(((3)))");
+    let mut a = vec![2, 0, 2, 1, 1, 0];
+    sort_colors(&mut a);
+    assert_eq!(a, vec![0, 0, 1, 1, 2, 2]);
 
-    let solutions = max_depth(num);
-    println!("{:?}", solutions);
+    let mut b = vec![2, 0, 1];
+    sort_colors(&mut b);
+    assert_eq!(b, vec![0, 1, 2]);
+
+    println!("All tests passed!");
 }
 
-pub fn max_depth(s: String) -> i32 {
-    let mut max_value: i32 = 0;
-    let mut current_depth = 0;
+pub fn sort_colors(nums: &mut Vec<i32>) {
+    const K: usize = 3;
 
-    for char in s.chars() {
-        match char {
-            '(' => {
-                current_depth += 1;
-                max_value = max_value.max(current_depth);
-            }
-            ')' => {
-                current_depth -= 1;
-            }
-            _ => continue
-        }
+    let mut counts = [0usize; K];
+
+    for &value in nums.iter() {
+        counts[value as usize] += 1;
     }
-    max_value
+
+    for i in 1..K {
+        counts[i] += counts[i - 1];
+    }
+
+    let mut output = vec![0; nums.len()];
+
+    for &x in nums.iter().rev() {
+        counts[x as usize] -= 1;
+
+        output[counts[x as usize]] = x;
+    }
+
+    nums.copy_from_slice(&output);
 }
