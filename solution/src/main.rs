2b37819f4d9bb7fabe9c3f31a3c9e5bb196461a4
Ludwin FM <ludwinss@gmail.com>
2025-10-22 13:55:50 -0400

    solution using greedy

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,21 +1,33 @@
 fn main() {
-    assert_eq!(find_closest(2, 7, 4), 1);
+    assert_eq!(max_diff(555), 888);
 
-    assert_eq!(find_closest(1, 5, 3), 0);
+    assert_eq!(max_diff(9), 8);
+
+    assert_eq!(max_diff(123456), 820000);
 
     println!("All tests passed!");
 }
 
-pub fn find_closest(x: i32, y: i32, z: i32) -> i32 {
-    match (x - z).abs().cmp(&(y - z).abs()) {
-        std::cmp::Ordering::Equal => {
-            return 0;
+pub fn max_diff(num: i32) -> i32 {
+    let num_str = num.to_string();
+
+    let max_num = num_str
+        .chars()
+        .find(|&c| c != '9')
+        .map_or(num_str.clone(), |c| num_str.replace(c, "9"));
+
+    let first_value = num_str.chars().next().unwrap();
+
+    let min_num = if first_value != '1' {
+        num_str.replace(first_value, "1")
+    } else {
+        match num_str.chars().skip(1).find(|&c| c != '0' && c != '1') {
+            Some(from) => num_str.replace(from, "0"),
+            None => num_str.clone(),
         }
-        std::cmp::Ordering::Greater => {
-            return 2;
-        }
-        std::cmp::Ordering::Less => {
-            return 1;
-        }
-    }
+    };
+
+    println!("{} {}", max_num, min_num);
+
+    max_num.parse::<i32>().unwrap() - min_num.parse::<i32>().unwrap()
 }
