807766ed3001d1ce5b3d866f36b451c583761742
Ludwin FM <ludwinss@gmail.com>
2025-10-25 04:03:57 -0400

    solution using botto Top-Down dynamic programing

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,23 +1,32 @@
+use std::collections::HashMap;
+
 fn main() {
-    assert_eq!(number_of_cuts(1), 0);
-    assert_eq!(number_of_cuts(2), 1);
-    assert_eq!(number_of_cuts(3), 3);
-    assert_eq!(number_of_cuts(4), 2);
-    assert_eq!(number_of_cuts(5), 5);
-    assert_eq!(number_of_cuts(6), 3);
-    assert_eq!(number_of_cuts(7), 7);
-    assert_eq!(number_of_cuts(8), 4);
-    assert_eq!(number_of_cuts(10), 5);
-    assert_eq!(number_of_cuts(100), 50);
+    assert_eq!(climb_stairs(1), 1);
+    assert_eq!(climb_stairs(2), 2);
+    assert_eq!(climb_stairs(3), 3);
+    assert_eq!(climb_stairs(4), 5);
     println!("All tests passed!");
 }
 
-pub fn number_of_cuts(n: i32) -> i32 {
-    if n == 1 {
-        return 0;
+pub fn climb_stairs(n: i32) -> i32 {
+    let mut memo: HashMap<i32, i32> = HashMap::new();
+
+    fn dn(n: i32, memo: &mut HashMap<i32, i32>) -> i32 {
+        if n <= 2 {
+            return n;
+        }
+
+        if let Some(memoized) = memo.get(&n) {
+            return *memoized;
+        }
+
+        let n_1 = dn(n - 1, memo);
+        let n_2 = dn(n - 2, memo);
+
+        let sum = n_1 + n_2;
+        memo.insert(n, sum);
+
+        sum
     }
-    match n % 2 {
-        0 => n / 2,
-        _ => n,
-    }
+    dn(n, &mut memo)
 }
