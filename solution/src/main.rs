a96895376a4f7152f9a9db4c1773497f24061a7a
Ludwin FM <ludwinss@gmail.com>
2025-11-05 22:20:21 -0400

    solution using maps

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,27 +1,47 @@
 fn main() {
-    assert_eq!(maximum_or(vec![12, 9], 1), 30);
-    assert_eq!(maximum_or(vec![8, 1, 2], 2), 35);
+    assert_eq!(
+        hardest_worker(10, vec![vec![0, 2], vec![2, 5], vec![0, 9], vec![1, 15]]),
+        1
+    );
+    assert_eq!(
+        hardest_worker(26, vec![vec![1, 1], vec![3, 7], vec![2, 12], vec![7, 17]]),
+        3
+    );
+    assert_eq!(hardest_worker(2, vec![vec![0, 10], vec![1, 10]]), 0);
+
+    assert_eq!(
+        hardest_worker(
+            70,
+            vec![
+                vec![36, 3],
+                vec![1, 5],
+                vec![12, 8],
+                vec![25, 9],
+                vec![53, 11],
+                vec![29, 12],
+                vec![52, 14]
+            ]
+        ),
+        12
+    );
     println!("All tests passed!");
 }
 
-pub fn maximum_or(nums: Vec<i32>, k: i32) -> i64 {
-    let len: usize = nums.len();
-    let nums_parsed: Vec<i64> = nums.iter().map(|&a| a as i64).collect();
+pub fn hardest_worker(_: i32, logs: Vec<Vec<i32>>) -> i32 {
+    let mut max_time = i32::MIN;
+    let mut prev_time = 0;
+    let mut result_id = logs[0][0];
 
-    //GREEDY CALC
-    let mut prefix: Vec<i64> = vec![0i64; len + 1];
-    let mut sufix: Vec<i64> = vec![0i64; len + 1];
+    for log in logs {
+        let [id, time] = log.try_into().unwrap();
+        let duration = time - prev_time;
 
-    for index in 1..=len {
-        prefix[index] = prefix[index - 1] | nums_parsed[index - 1];
+        if duration > max_time || (duration == max_time && id < result_id) {
+            max_time = duration;
+            result_id = id;
+        }
+        prev_time = time;
     }
 
-    for index in (0..len).rev() {
-        sufix[index] = sufix[index + 1] | nums_parsed[index];
-    }
-
-    (0..len)
-        .map(|index| prefix[index] | sufix[index + 1] | nums_parsed[index] << k)
-        .max()
-        .unwrap()
+    result_id
 }
