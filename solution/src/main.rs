8627ea43368676d3c4e155d4a79f3c5463ffcc6d
Ludwin FM <ludwinss@gmail.com>
2026-01-15 10:12:19 -0400

    solution using two pointers

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,28 +1,20 @@
 fn main() {
-    assert_eq!(fib(1), 1);
-    assert_eq!(fib(2), 1);
-    assert_eq!(fib(3), 2);
-    assert_eq!(fib(4), 3);
-    assert_eq!(fib(5), 5);
+    assert_eq!(
+        remove_duplicates(&mut vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4]),
+        5
+    );
+    assert_eq!(remove_duplicates(&mut vec![1, 1, 2]), 2);
     println!("All tests passed!");
 }
 
-pub fn fib(n: i32) -> i32 {
-    if n == 0 {
-        return 0;
+pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+    let mut write: usize = 1;
+
+    for read in 1..nums.len() {
+        if nums[read] != nums[read - 1] {
+            nums[write] = nums[read];
+            write += 1;
+        }
     }
-    if n <= 2 {
-        return 1;
-    }
-
-    let (mut prev, mut next) = (1, 2);
-
-    for _ in 3..n {
-        let curr = prev + next;
-
-        prev = next;
-        next = curr;
-    }
-
-    next
+    write as i32
 }
